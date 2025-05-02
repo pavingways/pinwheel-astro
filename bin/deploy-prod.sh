a894767295802a9db1b63f92d70c512ff215d6a8
Rocco Georgi <mail@roccogeorgi.com>
2025-05-02 23:28:57 +0200

    deploy PROD script added

---
+++ bin/deploy-prod.sh
@@ -0,0 +1,11 @@
+npm run build
+rsync -avr --delete-before  \
+                         --exclude '.*'  \
+                         --exclude '_deploy-dev.sh' \
+/Users/Rocco/Develop/pavingways/pavingways.com/dist/* /Users/Rocco/Develop/pavingways/pavingways.github.io/
+cp .gitignore /Users/Rocco/Develop/pavingways/pavingways.github.io/
+cd /Users/Rocco/Develop/pavingways/pavingways.github.io/
+git add .
+git commit -am "release $(date +'%Y-%m-%d')"
+git push origin main
+cd - || exit
